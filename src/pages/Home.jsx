3d3e08ab1c642625768d26a672399e78090ac5a0
kai2umbc <umbcwebsite123@gmail.com>
2025-02-25 13:37:45 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
                 <div className="relative">
                     <div className="w-full animate-slideDown">
                         <img
-                            src="kai2umbc/kai2umbc.github.io/src/img/front_page.jpg"
+                            src="./src/img/front_page.jpg"
                             alt="AI Brain Illustration"
                             className="w-full h-screen object-cover"
                             style={{
