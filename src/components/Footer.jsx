1351989f9995922a29ddeaa428bb0db506a9cd66
MatDawit <mathewiyob@gmail.com>
2025-02-25 16:07:36 -0500

    last fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -64,7 +64,7 @@ const Footer = () => {
                         <a href="/" onClick={(e) => handleNavClick(e, '/')}>
                             <div className="h-16 w-16">
                                 <img
-                                    src="/public/assets/Black KAI2 Logo.jpg"
+                                    src="/assets/Black KAI2 Logo.jpg"
                                     alt="KAI2 Logo"
                                     className="object-contain brightness-100 contrast-125"
                                 />
